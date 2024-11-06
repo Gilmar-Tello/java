dcb8532fbae5f7657d8761a33f2061abd32331be
Gilmar Tello Vásquez <93662620+Gilmar-Tello@users.noreply.github.com>
2024-11-06 17:46:27 -0600

    Add files via upload

---
+++ Evaluacion/src/evaluacion/Evaluacion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package evaluacion;
+
+/**
+ *
+ * @author tello
+ */
+public class Evaluacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("******************************");
+        System.out.println("****************************");
+        System.out.println("**************************");
+        System.out.println("************************");
+        System.out.println("**********************");
+        System.out.println("********************");
+        System.out.println("******************");
+        System.out.println("****************");
+    }
+    
+}
